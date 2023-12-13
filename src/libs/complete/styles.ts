35bd78ee23288514984c4a348bf467c72e00faac
cotist <tg.kim@alcherainc.com>
2023-12-13 16:00:06 +0900

    feat: complete 완성

---
+++ src/libs/complete/styles.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  background-color: #151515;
+  padding-bottom: 200px;
+`;
+
+export const Wrap = styled.div`
+  max-width: 1200px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const CompleteImg = styled.img`
+  margin-top: 102px;
+  margin-bottom: 64px;
+  width: 216px;
+  height: 233.999px;
+`;
+
+export const Title = styled.p`
+  margin: 0px;
+  color: #33cc99;
+  text-align: center;
+  font-family: Inter;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 50px; /* 125% */
+  margin-bottom: 18px;
+`;
+
+export const Context1 = styled.p`
+  margin: 0px;
+  color: #fff;
+  text-align: center;
+  font-family: Inter;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 30px; /* 150% */
+`;
+
+export const Context2 = styled.p`
+  white-space: pre-line;
+  color: #fff;
+  text-align: center;
+  font-family: Inter;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 30px; /* 150% */
+`;
